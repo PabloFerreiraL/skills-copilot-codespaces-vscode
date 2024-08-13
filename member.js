37a8c96aba89eab3a89034bb2d11f253c88c2ae5
Pablo Laranjo <pablo.laranjo@sinqia.com.br>
2024-08-13 17:01:20 -0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember(){
+        var member = [
+        {
+            name: "Aldo",
+            skills: ["HTML", "CSS", "JS"]
+        },
+        {
+            name: "Budi",
+            skills: ["PHP", "SQL"]
+        },
+        {
+            name: "Charlie",
+            skills: ["HTML", "JS"]
+        },
+        {
+            name: "Dika",
+            skills: ["CSS", "JS"]
+        },
+    ]
+
+    return member
+}
